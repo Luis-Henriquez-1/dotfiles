9254afda02cabe0a8caa442fe71288a93b09318d
Luis Henriquez-Perez <luis@luishp.xyz>
2020-06-02 23:46:21 -0400

    Change default search engine to qwant and add more engines.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,10 @@
 c.tabs.tabs_are_windows = True
-c.url.start_pages = "https://swisscows.com/"
-c.url.searchengines = {"DEFAULT" : "https://swisscows.com/web?query={}&region=en-US"}
+
+c.url.start_pages = "https://www.qwant.com/"
+
+# Set search engines to some of the intersting privacy respecting ones.
+# https://restoreprivacy.com/private-search-engine/
+c.url.searchengines = {"DEFAULT" : "https://www.qwant.com/?q={}&t=web",
+                       "mg" : "https://metager3.de/en/meta/meta.ger3?eingabe={}&submit-query=&focus=web&s=&f=&m=",
+                       "swiss" : "https://swisscows.com/web?query={}&region=en-US"
+                       "quant" : "https://www.qwant.com/?q={}&t=web"}
