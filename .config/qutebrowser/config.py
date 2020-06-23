6b56111fe707a3ceb622953b0746501db6b54be1
Luis Henriquez-Perez <luis@luishp.xyz>
2020-06-23 01:31:32 -0400

    Fix possible cause of performance issues with switch buffer.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -5,6 +5,7 @@ c.url.start_pages = "https://www.qwant.com/"
 # Set search engines to some of the intersting privacy respecting ones.
 # https://restoreprivacy.com/private-search-engine/
 c.url.searchengines = {"DEFAULT" : "https://www.qwant.com/?q={}&t=web",
-                       "mg" : "https://metager3.de/en/meta/meta.ger3?eingabe={}&submit-query=&focus=web&s=&f=&m=",
-                       "swiss" : "https://swisscows.com/web?query={}&region=en-US"
-                       "quant" : "https://www.qwant.com/?q={}&t=web"}
+                       # "mg" : "https://metager3.de/en/meta/meta.ger3?eingabe={}&submit-query=&focus=web&s=&f=&m=",
+                       # "swiss" : "https://swisscows.com/web?query={}&region=en-US"
+                       # "quant" : "https://www.qwant.com/?q={}&t=web"
+                       }
