d495dfe4fd0f98184bd19527751f49f6e803cb3d
Luis Henriquez-Perez <luis@luishp.xyz>
2021-01-16 12:58:52 -0500

    preliminary bootstrap code

--- .config/yadm/boostrap.sh
+++ .config/yadm/boostrap.sh
@@ -1,1 +1,10 @@
 # tangle boostrap.org
+emacs -Q --batch --eval "
+         (progn
+           (require 'ob-tangle)
+           (dolist (file command-line-args-left)
+             (with-current-buffer (find-file-noselect file)
+               (org-babel-tangle))))
+       " "$@"
+
+
