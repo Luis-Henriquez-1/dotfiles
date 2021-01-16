01010f5dc0808d2118d6fe37ad8e542333f97d2d
Luis Henriquez-Perez <luis@luishp.xyz>
2021-01-16 17:59:26 -0500

    Update boostrap code.

--- .config/yadm/boostrap.sh
+++ .config/yadm/boostrap.sh
@@ -1,10 +1,4 @@
 # tangle boostrap.org
-emacs -Q --batch --eval "
-         (progn
-           (require 'ob-tangle)
-           (dolist (file command-line-args-left)
-             (with-current-buffer (find-file-noselect file)
-               (org-babel-tangle))))
-       " "$@"
+emacs -Q --batch --eval "(progn (require 'org) (org-babel-load-file \"bootstrap.org\"))"
 
 
