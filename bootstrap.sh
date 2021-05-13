3ff913cef87307e9d6b5dd6355994be1f01ce40f
Luis Henriquez-Perez <luis@luishp.xyz>
2021-05-13 19:49:50 -0400

    Update boostrap file.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,19 +6,28 @@
 (require 'org)
 (require 'ob-lob)
 (require 'cl-lib)
+(require 'rx)
 
 (defun printf (format-string &rest args)
   "Same as print, but allows format."
   (print (apply #'format format-string args)))
 
 (defun main ()
-  (let* ((bootstrap-file (cl-find-if (apply-partially #'string-match "bootstrap\\.sh") command-line-args))
-	 (dotfiles-dir (file-name-directory bootstrap-file)))
-    (printf "Loading the library of babel from %s..." (expand-file-name "library-of-babel.org" dotfiles-dir))
-    (org-babel-lob-ingest (expand-file-name "library-of-babel.org" dotfiles-dir))
-    (printf "Tangling emacs config from %s..." (expand-file-name "emacs.org" dotfiles-dir))
-    (org-babel-tangle-file (expand-file-name "emacs.org" dotfiles-dir))
-    (printf "Done.")))
+  ;; Tangle my emacs files.
+  (dolist (file (directory-files "emacs/" t (rx (1+ anything) ".org")))
+    (printf "Tangling %s..." file)
+    (org-babel-tangle-file file))
+
+  (printf "Done!")
+  ;; Testing stuff
+  ;; (let* ((bootstrap-file (cl-find-if (apply-partially #'string-match "bootstrap\\.sh") command-line-args))
+  ;; 	 (dotfiles-dir (file-name-directory bootstrap-file)))
+  ;;   (printf "Loading the library of babel from %s..." (expand-file-name "library-of-babel.org" dotfiles-dir))
+  ;;   (org-babel-lob-ingest (expand-file-name "library-of-babel.org" dotfiles-dir))
+  ;;   (printf "Tangling emacs config from %s..." (expand-file-name "emacs.org" dotfiles-dir))
+  ;;   (org-babel-tangle-file (expand-file-name "emacs.org" dotfiles-dir))
+  ;;   (printf "Done."))
+  )
 
 ;; Local Variables:
 ;; mode: emacs-lisp
